6b7c3e496910617232031f510a7408f079ec5a6c
jorgearteiro <jorgearteiro@hotmail.com>
2021-12-18 01:01:17 +1100

    create eventclient

---
+++ sdk/meta/lists.cs
@@ -0,0 +1,19 @@
+﻿namespace meta;
+
+public interface IMetaList<T> : IList<T>   
+{
+    public IListMeta? Metadata { get; set; }
+    public List<T> Items { get; set; }
+}
+
+public interface IListMeta
+{
+    string? Continue { get; set; }
+    int? RemainingItemCount { get; set; }
+}
+
+public interface IListOptions
+{
+    string Continue { get; set; }
+    int Limit { get; set; }
+}
